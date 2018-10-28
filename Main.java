21beed531a97dcd59ba9089e960b93af31f8839e
helbaz <38982259+helbaz@users.noreply.github.com>
2018-10-28 20:11:17 +0100

    primer commit

--- Main.java
+++ Main.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class Main {
 
    public static void main(String[] args) {
-       int[] nums = new int[5];
+       int[] nums = new int[10];
        Scanner teclado = new Scanner(System.in);
        for (int i = 0; i < nums.length; i++) {
            System.out.println("Dime un numero");
            nums[i] = teclado.nextInt();
        }
        Arrays.sort(nums);
-       System.out.println("El més petit és "+nums[0]);
+       System.out.println("El més petit és "+nums[0]+" i el més gran "+nums[nums.length]));
    }
 }
 
